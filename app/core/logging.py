89c78ab4ef71321e0c2f4f074b6aace937f46c3d
Haris0059 <haris.skeledzija@stu.ibu.edu.ba>
2026-04-19 17:58:20 +0200

    Add basic structlog configuration

---
+++ app/core/logging.py
@@ -0,0 +1,19 @@
+import structlog
+import logging
+
+def setup_logging():
+    structlog.configure(
+        processors=[
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.add_logger_name,
+            structlog.processors.JSONRenderer()
+        ],
+        wrapper_class=structlog.stdlib.BoundLogger,
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=False
+    )
+    logging.basicConfig(format="%(message)s", level=logging.INFO)
+
+setup_logging()
